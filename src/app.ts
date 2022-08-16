80f01405c92d9a65c7bcfd19c678f81bd14eac4b
mikiyaaa <katocha.m0220thankyou@gmail.com>
2022-08-16 19:02:26 +0900

    サーバーからのデータをフロントエンド用のクラスに生成
    バックエンドから送られてきたデータを、Productクラスとして生成

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,24 @@
 import _ from 'lodash';
+import { Product } from './product.model';
 
-console.log(_.shuffle([1 ,2 ,3]));
-console.log('auto build');
+// サーバーから送られてきたデータ
+const products = [
+    {
+        title: '商品1',
+        price: 100,
+    },
+    {
+        title: '商品2',
+        price: 200,
+    }
+];
+
+// Productクラスのインスタンスで再生成
+const loadedProducts = products.map(prod => {
+    return new Product(prod.title, prod.price);
+});
+
+for (const prod of loadedProducts) {
+    console.log(prod.getInformation());
+}
+
