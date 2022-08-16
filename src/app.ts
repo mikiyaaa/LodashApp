542b47ffcef0f14e4aa1f5f12dd55cccad525c3a
mikiyaaa <katocha.m0220thankyou@gmail.com>
2022-08-16 19:15:01 +0900

    'classTransformer'をインポートして、Productクラスに変換
    classTransformerはCommonJS形式で動作する

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
+import 'reflect-metadata';
+import { plainToInstance } from 'class-transformer';
 import _ from 'lodash';
+
 import { Product } from './product.model';
 
 // サーバーから送られてきたデータ
 const products = [
     {
         title: '商品1',
-        price: 100,
+        price: 300,
     },
     {
         title: '商品2',
@@ -14,11 +17,13 @@ const products = [
 ];
 
 // Productクラスのインスタンスで再生成
-const loadedProducts = products.map(prod => {
-    return new Product(prod.title, prod.price);
-});
+// const loadedProducts = products.map(prod => {
+//     return new Product(prod.title, prod.price);
+// });
+
+// サードパーティ'classTransformer'でProductクラスに変換
+const loadedProducts = plainToInstance(Product, products);
 
 for (const prod of loadedProducts) {
     console.log(prod.getInformation());
 }
-
